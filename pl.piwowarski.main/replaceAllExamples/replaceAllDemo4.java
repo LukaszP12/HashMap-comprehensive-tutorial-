d5f00833306b4dab884ab4f56f285b5ddcce36d7
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-11-02 18:00:54 +0100

    Map.of() does not allow nulls

--- pl.piwowarski.main/replaceAllExamples/replaceAllDemo4.java
+++ pl.piwowarski.main/replaceAllExamples/replaceAllDemo4.java
@@ -5,14 +5,12 @@ import java.util.Map;
 
 class replaceAllDemo4 {
     public static void main(String[] args) {
-        Map<String, String> status = new HashMap<>(Map.of(
-                "Task1", "Done",
-                "Task2", null,
-                "Task3", "Pending"
-        ));
+        Map<String, String> status = new HashMap<>();
+        status.put("Task1", "Done");
+        status.put("Task2", null);
+        status.put("Task3", "Pending");
 
         status.replaceAll((task, value) -> value == null ? "Unknown" : value);
-
         System.out.println(status);
     }
 }
